9b7337e1bae9fa00099ede744e0096ac815e33d2
ai-develop <ai-develop@asial.co.jp>
2022-05-11 19:26:45 +0900

    20220511

--- rect_eval.sh
+++ rect_eval.sh
@@ -6,4 +6,4 @@
 
 
 # English/Hungarian model
-python3 -m paddle.distributed.launch --gpus '0,1,2,3,4,5' tools/eval.py -c configs/rec/multi_language/rec_en_number_hungarian_lite_eval.yml -o Global.checkpoints=trained_model/eng/8_6/best_accuracy
+python3 -m paddle.distributed.launch --gpus '0,1' tools/eval.py -c configs/rec/multi_language/rec_en_number_hungarian_lite_eval.yml -o Global.checkpoints=trained_model/eng/8_6/best_accuracy
--- rect_train.sh
+++ rect_train.sh
@@ -1,5 +1,5 @@
 # english/hungarian
-python3 -m paddle.distributed.launch --gpus '0,1,2,3,4,5'  tools/train.py -c configs/rec/multi_language/rec_en_number_hungarian_lite_train.yml
+python3 -m paddle.distributed.launch --gpus '0,1'  tools/train.py -c configs/rec/multi_language/rec_en_number_hungarian_lite_train.yml
 
 # japanese
 # python3 -m paddle.distributed.launch --gpus '0,1,2,3'  tools/train.py -c configs/rec/multi_language/rec_japan_lite_train.yml
